214fd7de405cdd25af718e7d6f9ea56d095c0ab9
Thang Bach <rustdbach20004@gmail.com>
2026-08-04 12:00:30 +0700

    fix: ignore public directory in ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import js from "@eslint/js";
 import prettier from "eslint-plugin-prettier/recommended";
 
 export default [
-  { ignores: ["dist"] },
+  { ignores: ["dist", "public", "public/**", "**/wink-bridge.js"] },
   js.configs.recommended,
   prettier,
   {
